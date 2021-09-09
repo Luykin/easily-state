da069d5233ba3ac930fa1d8ba3f07ca3951e4b62
luykin <luo.yukun@tslsmart.com>
2021-09-09 14:25:29 +0800

    feat: easy-state

--- easy-state.js
+++ easy-state.js
@@ -1,5 +1,5 @@
 "use strict";
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.cache = exports.setGlobalStorage = exports.getGlobal = exports.setGlobal = exports.bindData = exports.init = void 0;
 var R = require("ramda");
 var CryptoJS = require("crypto-js");
@@ -13,7 +13,8 @@ var CryptoJS = require("crypto-js");
  */
 var DFT_VERSION = "@VERSION_BATE@";
 var LSV_KEY = "@WEB_VERSION_EASY_STATE@";
-var LOCAL_CACHE = {}, GLOBAL = {};
+var DESTROY_KEY = "@es_destruction_identification";
+var LOCAL_CACHE = {}, GLOBAL = {}, PROXY_GLOBAL = {};
 var IS_CONSOLE = false;
 var WEB_VERSION = R.defaultTo(DFT_VERSION, localStorage.getItem(LSV_KEY));
 /**
@@ -24,7 +25,6 @@ function init(group, config) {
         IS_CONSOLE = Boolean(config === null || config === void 0 ? void 0 : config.console);
         _checkVS(config === null || config === void 0 ? void 0 : config.version);
         var UDG = R.type(group) === "Array" ? R.mergeAll(group) : group; // un formatted default global data 未格式化初始数据
-        console.log(UDG, "UDG", IS_CONSOLE);
         var isCacheKey = function (val, key) { return key.includes(WEB_VERSION); };
         var notCacheKey = function (val, key) { return !key.includes(WEB_VERSION); };
         var noCacheDG = R.pickBy(notCacheKey);
@@ -38,6 +38,7 @@ function init(group, config) {
         };
         var cacheDG = R.pipe(R.pickBy(isCacheKey), R.toPairs, R.map(changeKey), R.fromPairs);
         R.isEmpty(GLOBAL) && (GLOBAL = R.mergeAll([noCacheDG(UDG), cacheDG(UDG)]));
+        _proxyGlobal(); //TODO 在这实现总的监听事件
     }
     catch (err) {
         _consoleLog(JSON.stringify(err));
@@ -45,36 +46,29 @@ function init(group, config) {
 }
 exports.init = init;
 /**
- * 给每个key添加代理
- * @param key
+ * PROXY_GLOBAL添加代理
  */
-function _proxy(key) {
-    try {
-        var val_1 = GLOBAL[key];
-        Object.defineProperty(GLOBAL, key, {
-            set: function (newVal) {
-                if (newVal !== val_1 && LOCAL_CACHE[key]) {
-                    console.log(LOCAL_CACHE[key], "xx");
-                    LOCAL_CACHE[key].forEach(function (noticeFnc) { return noticeFnc(key, newVal); });
-                    val_1 = newVal;
-                    this[key] = newVal;
-                }
+function _proxyGlobal() {
+    // @ts-ignore
+    PROXY_GLOBAL = new Proxy(GLOBAL, {
+        get: function (target, prop) {
+            return GLOBAL[prop];
+        },
+        set: function (target, prop, value) {
+            var _a;
+            if (value !== GLOBAL[prop]) {
+                target[prop] = value;
+                (_a = LOCAL_CACHE[prop]) === null || _a === void 0 ? void 0 : _a.forEach(function (noticeFnc) { return noticeFnc(prop, value); });
             }
-        });
-    }
-    catch (err) {
-        _consoleLog(JSON.stringify(err));
-    }
+            return true;
+        }
+    });
 }
 function bindData(key, local) {
     try {
-        console.log("????");
         _clearDestroyLocal(key); //清空已经被销毁的订阅者
         var noticeFnc_1 = _buildNotice(local); //先处理好noticeFnc
-        R.ifElse(function () { return !(LOCAL_CACHE[key]); }, function () { return LOCAL_CACHE[key] = [noticeFnc_1]; }, function () { return LOCAL_CACHE[key].push(noticeFnc_1); })();
-        _proxy(key);
-        _wrapDestroy(local);
-        console.log(R.type(local) === "Array" && (local === null || local === void 0 ? void 0 : local.length) === 2, "a");
+        R.ifElse(function () { return !LOCAL_CACHE[key]; }, function () { return LOCAL_CACHE[key] = [noticeFnc_1]; }, function () { return LOCAL_CACHE[key].push(noticeFnc_1); })();
         return R.ifElse(function () { return R.type(local) === "Array" && (local === null || local === void 0 ? void 0 : local.length) === 2; }, function () { return [GLOBAL[key] || local[0], local[1]]; }, function () { return GLOBAL[key]; })();
     }
     catch (err) {
@@ -85,18 +79,16 @@ function bindData(key, local) {
 exports.bindData = bindData;
 function setGlobal(key, value, callback) {
     try {
-        GLOBAL[key] = value;
-        console.log(key, GLOBAL);
+        PROXY_GLOBAL[key] = value;
         callback && callback();
     }
     catch (err) {
-        console.log(err);
         _consoleLog(JSON.stringify(err));
     }
 }
 exports.setGlobal = setGlobal;
 function getGlobal(key) {
-    return GLOBAL[key];
+    return PROXY_GLOBAL[key];
 }
 exports.getGlobal = getGlobal;
 function setGlobalStorage(key, value, otherKey) {
@@ -106,49 +98,44 @@ function setGlobalStorage(key, value, otherKey) {
 exports.setGlobalStorage = setGlobalStorage;
 function _clearDestroyLocal(key) {
     try {
-        var isNotDestroy_1 = function (l) { return !l[LSV_KEY]; }; //判定没有被销毁的订阅者 !false;
-        R.ifElse(R.isEmpty(LOCAL_CACHE[key]), R.identity, function () { return (LOCAL_CACHE[key] = R.filter(isNotDestroy_1, LOCAL_CACHE[key])); })(key);
-    }
-    catch (err) {
-        _consoleLog(JSON.stringify(err));
-    }
-}
-function _wrapDestroy(local) {
-    try {
-        if (R.type(local) === "Array")
-            return;
-        if (R.type(local === null || local === void 0 ? void 0 : local.setState) === "Function") { //组件销毁
-            local.componentWillUnmount = function () {
-                local === null || local === void 0 ? void 0 : local.componentWillUnmount.call(local);
-                local[LSV_KEY] = true;
-            };
-            return;
-        }
-        local.beforeDestroy = function () {
-            local === null || local === void 0 ? void 0 : local.beforeDestroy.call(local);
-            local[LSV_KEY] = true;
-        };
+        var isNotDestroy = function (l) { return !l[DESTROY_KEY]; }; //判定没有被销毁的订阅者 !false;
+        LOCAL_CACHE[key] && (LOCAL_CACHE[key] = R.filter(isNotDestroy, LOCAL_CACHE[key]));
     }
     catch (err) {
         _consoleLog(JSON.stringify(err));
     }
 }
 function _buildNotice(local) {
-    var isDestroy = local[LSV_KEY];
     if (R.type(local) === "Array") { // react hook
-        return function (key, newVal) {
-            console.log("执行 Array");
-            R.ifElse(R.always(!isDestroy && (local === null || local === void 0 ? void 0 : local.length)), local[1], function () { return local[LSV_KEY] = true; })(newVal);
+        var update2reactHook_1 = function (key, newVal) {
+            local[1](newVal);
+            update2reactHook_1[DESTROY_KEY] = true;
         };
+        return update2reactHook_1;
     }
     if (R.type(local === null || local === void 0 ? void 0 : local.setState) === "Function") { // react class
-        console.log("执行 Function");
-        return function (key, newVal) {
+        var update2reactClass = function (key, newVal) {
             var _a;
-            return !isDestroy && local["setState"]((_a = {}, _a[key] = newVal, _a));
+            local["setState"]((_a = {}, _a[key] = newVal, _a));
+        };
+        _addDestroy(local, "componentWillUnmount", update2reactClass);
+        return update2reactClass;
+    }
+    var update2vue = function (key, newVal) {
+        local && (local[key] = newVal);
+    };
+    _addDestroy(local, "beforeDestroy", update2vue);
+    return update2vue;
+}
+function _addDestroy(local, fncName, update) {
+    if (!local[DESTROY_KEY]) { //如果没有添加过销毁清除
+        local[DESTROY_KEY] = "Cleanup function has been added"; //添加标记
+        var destroy_1 = local[fncName];
+        local[fncName] = function () {
+            destroy_1 && destroy_1.call(local);
+            update[DESTROY_KEY] = true;
         };
     }
-    return function (key, newVal) { return !isDestroy && local && (local[key] = newVal); }; //vue 2/3
 }
 /**
  * 判断版本,新版本：清空localStorage,重置版本号
@@ -160,7 +147,6 @@ function _checkVS(version) {
         localStorage.setItem(LSV_KEY, v);
         WEB_VERSION = v;
     };
-    console.log(version, WEB_VERSION);
     R.ifElse(R.equals(WEB_VERSION), R.identity, clear)(version);
 }
 function _consoleLog(info) {
@@ -170,12 +156,3 @@ function cache(name) {
     return "" + WEB_VERSION + name;
 }
 exports.cache = cache;
-// export const init = init;
-// export default {
-//   init,
-//   cache,
-//   bindData,
-//   setGlobal,
-//   getGlobal,
-//   setGlobalStorage
-// };
--- easy-state.ts
+++ easy-state.ts
@@ -11,7 +11,8 @@ import * as CryptoJS from "crypto-js";
  */
 let DFT_VERSION: string = "@VERSION_BATE@";
 let LSV_KEY: string = "@WEB_VERSION_EASY_STATE@";
-let LOCAL_CACHE: object = {}, GLOBAL: object = {};
+let DESTROY_KEY: string = "@es_destruction_identification";
+let LOCAL_CACHE: object = {}, GLOBAL: object = {}, PROXY_GLOBAL: object = {};
 let IS_CONSOLE: boolean = false;
 let WEB_VERSION: string = R.defaultTo(DFT_VERSION, localStorage.getItem(LSV_KEY));
 
@@ -24,7 +25,6 @@ export function init(group: Array<object> | object, config?: { version: string, 
     IS_CONSOLE = Boolean(config?.console);
     _checkVS(config?.version);
     const UDG = R.type(group) === "Array" ? R.mergeAll(group) : group; // un formatted default global data 未格式化初始数据
-    console.log(UDG, "UDG", IS_CONSOLE);
     const isCacheKey = (val, key) => key.includes(WEB_VERSION);
     const notCacheKey = (val, key) => !key.includes(WEB_VERSION);
     const noCacheDG = R.pickBy(notCacheKey);
@@ -38,42 +38,36 @@ export function init(group: Array<object> | object, config?: { version: string, 
     };
     const cacheDG = R.pipe(R.pickBy(isCacheKey), R.toPairs, R.map(changeKey), R.fromPairs);
     R.isEmpty(GLOBAL) && (GLOBAL = R.mergeAll([noCacheDG(UDG), cacheDG(UDG)]));
+    _proxyGlobal();//TODO 在这实现总的监听事件
   } catch (err) {
     _consoleLog(JSON.stringify(err));
   }
 }
 
 /**
- * 给每个key添加代理
- * @param key
+ * PROXY_GLOBAL添加代理
  */
-function _proxy(key: string) {
-  try {
-    let val = GLOBAL[key];
-    Object.defineProperty(GLOBAL, key, {
-      set: function(newVal) {  //通知各个订阅者，支持[react class / hook] [vue2 / 3]
-        if (newVal !== val && LOCAL_CACHE[key]) {
-          console.log(LOCAL_CACHE[key], "xx");
-          LOCAL_CACHE[key].forEach((noticeFnc) => noticeFnc(key, newVal));
-          val = newVal;
-          this[key] = newVal;
-        }
+function _proxyGlobal() {
+  // @ts-ignore
+  PROXY_GLOBAL = new Proxy(GLOBAL, {
+    get: function(target, prop) {
+      return GLOBAL[prop];
+    },
+    set: function(target, prop, value) {
+      if (value !== GLOBAL[prop]) {
+        target[prop] = value;
+        LOCAL_CACHE[prop]?.forEach((noticeFnc) => noticeFnc(prop, value));
       }
-    });
-  } catch (err) {
-    _consoleLog(JSON.stringify(err));
-  }
+      return true;
+    }
+  });
 }
 
 export function bindData(key: string, local: any) {
   try {
-    console.log("????");
     _clearDestroyLocal(key);//清空已经被销毁的订阅者
     const noticeFnc = _buildNotice(local); //先处理好noticeFnc
-    R.ifElse(() => !(LOCAL_CACHE[key]), () => LOCAL_CACHE[key] = [noticeFnc], () => LOCAL_CACHE[key].push(noticeFnc))();
-    _proxy(key);
-    _wrapDestroy(local);
-    console.log(R.type(local) === "Array" && local?.length === 2, "a");
+    R.ifElse(() => !LOCAL_CACHE[key], () => LOCAL_CACHE[key] = [noticeFnc], () => LOCAL_CACHE[key].push(noticeFnc))();
     return R.ifElse(() => R.type(local) === "Array" && local?.length === 2, () => [GLOBAL[key] || local[0], local[1]], () => GLOBAL[key])();
   } catch (err) {
     _consoleLog(JSON.stringify(err));
@@ -83,17 +77,15 @@ export function bindData(key: string, local: any) {
 
 export function setGlobal(key: string, value: any, callback?) {
   try {
-    GLOBAL[key] = value;
-    console.log(key, GLOBAL);
+    PROXY_GLOBAL[key] = value;
     callback && callback();
   } catch (err) {
-    console.log(err);
     _consoleLog(JSON.stringify(err));
   }
 }
 
 export function getGlobal(key: string) {
-  return GLOBAL[key];
+  return PROXY_GLOBAL[key];
 }
 
 export function setGlobalStorage(key: string, value: any, otherKey?: string) {
@@ -103,45 +95,44 @@ export function setGlobalStorage(key: string, value: any, otherKey?: string) {
 
 function _clearDestroyLocal(key: string) {
   try {
-    const isNotDestroy = l => !l[LSV_KEY]; //判定没有被销毁的订阅者 !false;
-    R.ifElse(R.isEmpty(LOCAL_CACHE[key]), R.identity, () => (LOCAL_CACHE[key] = R.filter(isNotDestroy, LOCAL_CACHE[key])))(key);
-  } catch (err) {
-    _consoleLog(JSON.stringify(err));
-  }
-}
-
-function _wrapDestroy(local: any) {
-  try {
-    if (R.type(local) === "Array") return;
-    if (R.type(local?.setState) === "Function") { //组件销毁
-      local.componentWillUnmount = () => {
-        local?.componentWillUnmount.call(local);
-        local[LSV_KEY] = true;
-      };
-      return;
-    }
-    local.beforeDestroy = () => { //vue2 TODO vue3 onUnmounted
-      local?.beforeDestroy.call(local);
-      local[LSV_KEY] = true;
-    };
+    const isNotDestroy = l => !l[DESTROY_KEY]; //判定没有被销毁的订阅者 !false;
+    LOCAL_CACHE[key] && (LOCAL_CACHE[key] = R.filter(isNotDestroy, LOCAL_CACHE[key]));
   } catch (err) {
     _consoleLog(JSON.stringify(err));
   }
 }
 
 function _buildNotice(local: any) {
-  const isDestroy = local[LSV_KEY];
   if (R.type(local) === "Array") { // react hook
-    return (key, newVal) => { //拿不到hook数组就清理local list
-      console.log("执行 Array");
-      R.ifElse(R.always(!isDestroy && local?.length), local[1], () => local[LSV_KEY] = true)(newVal);
+    const update2reactHook = function(key, newVal) {
+      local[1](newVal);
+      update2reactHook[DESTROY_KEY] = true;
     };
+    return update2reactHook;
   }
   if (R.type(local?.setState) === "Function") { // react class
-    console.log("执行 Function");
-    return (key, newVal) => !isDestroy && local["setState"]({[key]: newVal});
+    const update2reactClass = function(key, newVal) {
+      local["setState"]({[key]: newVal});
+    };
+    _addDestroy(local, "componentWillUnmount", update2reactClass);
+    return update2reactClass;
   }
-  return (key, newVal) => !isDestroy && local && (local[key] = newVal); //vue 2/3
+  const update2vue = function(key, newVal) { //vue 2/3
+    local && (local[key] = newVal);
+  };
+  _addDestroy(local, "beforeDestroy", update2vue);
+  return update2vue;
+}
+
+function _addDestroy(local: any, fncName: string, update?) {
+  if (!local[DESTROY_KEY]) { //如果没有添加过销毁清除
+    local[DESTROY_KEY] = "Cleanup function has been added";//添加标记
+    const destroy = local[fncName];
+    local[fncName] = () => {
+      destroy && destroy.call(local);
+      update[DESTROY_KEY] = true;
+    };
+  }
 }
 
 /**
@@ -153,7 +144,6 @@ function _checkVS(version: string = DFT_VERSION) {
     localStorage.setItem(LSV_KEY, v);
     WEB_VERSION = v;
   };
-  console.log(version, WEB_VERSION);
   R.ifElse(R.equals(WEB_VERSION), R.identity, clear)(version);
 }
 
@@ -164,13 +154,3 @@ function _consoleLog(info: string) {
 export function cache(name: string) {
   return `${WEB_VERSION}${name}`;
 }
-
-// export const init = init;
-// export default {
-//   init,
-//   cache,
-//   bindData,
-//   setGlobal,
-//   getGlobal,
-//   setGlobalStorage
-// };
